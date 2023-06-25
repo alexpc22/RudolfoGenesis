604778f77a4a38561a9d8809b9c8238b9ca8eb99
Alexandre Castro <alexandrepaiva3001@gmail.com>
2023-06-25 16:24:23 -0300

    Add Aula1.

---
+++ aula1.py
@@ -0,0 +1,20 @@
+# '#' Permite escrever um comentário
+print('123') # Pode escrever assim também.
+
+# Pode ser assim também ...
+
+"""
+Tudo o que estiver aqui dentro, será um comentário.
+"""
+
+nome = 'Alex'
+print(nome)
+
+# Imprima um número
+# imprimir(123) 
+""" Estou adorando Python """
+print ( 'Agora sou dev.' )
+#
+
+print(1234)
+
